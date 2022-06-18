3a843fd819368ae57e5ef7194b176649d872962c
Gwyn <gwyn@playtechnique.io>
2022-06-18 10:52:26 -0600

    Adds docker rm for the hackerss container to create_container, as I keep forgetting to do this on the server

--- create_container.sh
+++ create_container.sh
@@ -2,4 +2,5 @@
 
 # Script for creating a container from the hackerss image.
 source hackerss.env.sh
+docker rm hackerss || true
 docker create -p ${RSS_FEED_PORT}:${RSS_FEED_PORT} --expose ${RSS_FEED_PORT} --name hackerss -e STORIES_LIMIT=25 docker.io/jamandbees/hackerss:0.0.4
